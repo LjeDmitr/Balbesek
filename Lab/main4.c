8879f68dae8c519a83a0acae9b4ce2ff4a09f073
Dmitry Stacenko <78585841+LjeDmitr@users.noreply.github.com>
2022-04-18 18:58:34 +0700

    Add files via upload

--- Lab/main4.c
+++ Lab/main4.c
@@ -1,16 +1,65 @@
 #include "BigChars.h"
 #include "SC.h"
 #include "myReadkey.h"
+#include <signal.h>
+
+void bigCharPrint(int index);
+static struct Bigchars number[] = {{1717976064, 3958374},
+								   {1010315264, 3158064},
+								   {1010842624, 8258050},
+								   {2120252928, 8282238},
+								   {2120640000, 6316158},
+								   {1040350720, 4079680},
+								   {35789824, 1974814},
+								   {811630080, 396312},
+								   {1013332992, 3958374},
+								   {2087074816, 3956832}};
+
+void printIC(int x, int y, int index)
+{
+	mt_gotoXY(x, y);
+	printf("+");
+	for (int k = 0; k < 4; k++)
+	{
+		mt_gotoXY(x + 4 - k, y);
+		printf("%d", index % 10);
+		index /= 10;
+	}
+}
+
+void printAC(int x, int y, int index)
+{
+	mt_gotoXY(x, y);
+	printf("+");
+	int tmpRam = RAM[index];
+	for (int k = 0; k < 4; k++)
+	{
+		mt_gotoXY(x + 4 - k, y);
+		printf("%d", tmpRam % 10);
+		tmpRam /= 10;
+	}
+}
+
+void printRam(int x, int y, int index)
+{
+	mt_gotoXY(x, y);
+	printf("+");
+	int tmpRam = RAM[index];
+	for (int k = 0; k < 4; k++)
+	{
+		mt_gotoXY(x + 4 - k, y);
+		printf("%d", tmpRam % 10);
+		tmpRam /= 10;
+	}
+}
 
 void interface()
 {
 	mt_clear();
 	bc_box(1, 1, 61, 12);
-	sc_memoryInit();
 	mt_gotoXY(30, 1);
 	printf("Memory");
 	int index = 0, tmpRam = 0;
-	RAM[index] = 3506;
 	for (int i = 1; i <= 10; i++)
 	{
 		for (int j = 0; j < 60; j += 6)
@@ -40,7 +89,7 @@ void interface()
 	mt_gotoXY(70, 1);
 	printf("accumulator");
 	mt_gotoXY(72, 2);
-	printf("+9999");
+	printf("+0000");
 
 	bc_box(64, 4, 22, 3);
 	mt_gotoXY(67, 4);
@@ -60,17 +109,6 @@ void interface()
 	mt_gotoXY(72, 11);
 	printf("O E V M\n");
 
-	struct Bigchars number[] = {{1717976064, 3958374},
-								{1010315264, 3158064},
-								{1010842624, 8258050},
-								{2120252928, 8282238},
-								{2120640000, 6316158},
-								{1040350720, 4079680},
-								{35789824, 1974814},
-								{811630080, 396312},
-								{1013332992, 3958374},
-								{2087074816, 3956832}};
-
 	bc_box(1, 13, 46, 10);
 	int bigCh[2];
 	bigCh[0] = 2115508224;
@@ -79,16 +117,7 @@ void interface()
 	bc_printbigchar(bigCh, x, y, red, cl_default);
 
 	index = 0;
-	tmpRam = RAM[index];
-	x = 38;
-	for (int i = 0; i < 4; i++)
-	{
-		bigCh[0] = number[tmpRam % 10].value1;
-		bigCh[1] = number[tmpRam % 10].value2;
-		bc_printbigchar(bigCh, x, y, red, cl_default);
-		tmpRam /= 10;
-		x -= 9;
-	}
+	bigCharPrint(index);
 
 	mt_setfgcolor(cl_default);
 	mt_setbgcolor(cl_default);
@@ -112,65 +141,129 @@ void interface()
 	printf("\n");
 }
 
+void bigCharPrint(int index)
+{
+	int bigCh[2];
+	int tmpRam = RAM[index];
+	int x = 38, y = 14;
+	for (int i = 0; i < 4; i++)
+	{
+		bigCh[0] = number[tmpRam % 10].value1;
+		bigCh[1] = number[tmpRam % 10].value2;
+		bc_printbigchar(bigCh, x, y, red, cl_default);
+		tmpRam /= 10;
+		x -= 9;
+	}
+	mt_setfgcolor(cl_default);
+	mt_setbgcolor(cl_default);
+}
+
 int main()
 {
+	setbuf(stdout, 0);
+	sc_memoryInit();
 	interface();
 	int key = -1;
-	int x = 2, y = 2;
-	int count_n = 0;
-	char buf[30];
+	int x = 2, y = 2, accumulator = 0, instructionCounter = 0;
 	mt_gotoXY(x, y);
 	while (key != 13)
 	{
 		if (rk_readkey(&key) == -1)
 		{
+			mt_clear();
 			printf("Error term\n");
 			return 0;
 		}
 		if (key == KEY_up && y > 2)
 		{
 			y--;
-			count_n = sprintf(buf, "\E[%d;%dH", y, x);
-			write(STDIN_FILENO, buf, count_n);
+			mt_gotoXY(x, y);
+			bigCharPrint((y - 2) * 10 + (x - 2) / 6);
+			printIC(72, 5, (y - 2) * 10 + (x - 2) / 6);
+			printAC(72, 2, (y - 2) * 10 + (x - 2) / 6);
+			mt_gotoXY(x, y);
 		}
 		else if (key == KEY_down && y < 11)
 		{
 			y++;
-			count_n = sprintf(buf, "\E[%d;%dH", y, x);
-			write(STDIN_FILENO, buf, count_n);
+			mt_gotoXY(x, y);
+			bigCharPrint((y - 2) * 10 + (x - 2) / 6);
+			printIC(72, 5, (y - 2) * 10 + (x - 2) / 6);
+			printAC(72, 2, (y - 2) * 10 + (x - 2) / 6);
+			mt_gotoXY(x, y);
 		}
-		else if (key == KEY_right && x < 60)
+		else if (key == KEY_right && x < 56)
 		{
 			x += 6;
-			count_n = sprintf(buf, "\E[%d;%dH", y, x);
-			write(STDIN_FILENO, buf, count_n);
+			mt_gotoXY(x, y);
+			bigCharPrint((y - 2) * 10 + (x - 2) / 6);
+			printIC(72, 5, (y - 2) * 10 + (x - 2) / 6);
+			printAC(72, 2, (y - 2) * 10 + (x - 2) / 6);
+			mt_gotoXY(x, y);
 		}
 		else if (key == KEY_left && x > 2)
 		{
 			x -= 6;
-			count_n = sprintf(buf, "\E[%d;%dH", y, x);
-			write(STDIN_FILENO, buf, count_n);
+			mt_gotoXY(x, y);
+			bigCharPrint((y - 2) * 10 + (x - 2) / 6);
+			printIC(72, 5, (y - 2) * 10 + (x - 2) / 6);
+			printAC(72, 2, (y - 2) * 10 + (x - 2) / 6);
+			mt_gotoXY(x, y);
 		}
 		else if (key == KEY_enter)
 		{
-			write(STDIN_FILENO, "     ", 5);
-			count_n = sprintf(buf, "\E[%d;%dH", y, x);
-			write(STDIN_FILENO, buf, count_n);
-			count_n = sprintf(buf, "+", y, x);
-			write(STDIN_FILENO, buf, count_n);
+			printf("+    ");
+			mt_gotoXY(x + 1, y);
+			scanf("%4d", &(RAM[(y - 2) * 10 + (x - 2) / 6]));
+			bigCharPrint((y - 2) * 10 + (x - 2) / 6);
+			printIC(72, 5, (y - 2) * 10 + (x - 2) / 6);
+			printAC(72, 2, (y - 2) * 10 + (x - 2) / 6);
+			mt_gotoXY(x, y);
 		}
 		else if (key == KEY_l)
 		{
-			rk_mytermrestore();
+			sc_memoryInit();
+			sc_memoryLoad("Ram.bin");
+			interface();
 		}
 		else if (key == KEY_s)
 		{
-			rk_mytermsave();
+			sc_memorySave("Ram.bin");
+		}
+		else if (key == KEY_i)
+		{
+			sc_memoryInit();
+			interface();
 		}
 		else if (key == KEY_f5)
 		{
+			mt_gotoXY(72, 2);
+			printf("+    ");
+			mt_gotoXY(73, 2);
+			scanf("%d", &accumulator);
+			mt_gotoXY(x, y);
+			RAM[(y - 2) * 10 + (x - 2) / 6] = accumulator;
+			printRam(x, y, (y - 2) * 10 + (x - 2) / 6);
+			mt_gotoXY(x, y);
+			bigCharPrint((y - 2) * 10 + (x - 2) / 6);
+			mt_gotoXY(x, y);
 		}
-		// rk_mytermregime(1, 0, 0, 0, 0);
+		else if (key == KEY_f6)
+		{
+			mt_gotoXY(72, 5);
+			printf("+    ");
+			mt_gotoXY(73, 5);
+			scanf("%d", &instructionCounter);
+			if (instructionCounter > 99)
+			{
+				instructionCounter = 0;
+				printIC(72, 5, 0);
+			}
+			y = instructionCounter / 10 + 2;
+			x = (instructionCounter % 10) * 6 + 2;
+			bigCharPrint((y - 2) * 10 + (x - 2) / 6);
+			mt_gotoXY(x, y);
+		}
 	}
 
 	return 0;
